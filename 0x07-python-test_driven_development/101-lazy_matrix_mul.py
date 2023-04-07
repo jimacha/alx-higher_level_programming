bebb6e41f919e3b5393a9e729f7060e56f9d6365
jimacha <machariajimmy68@gmail.com>
2023-04-07 13:11:58 -0700

    test drive few corrections

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,15 @@
-#!/usr/bin/python3
-"""
-lazy matrix multiplication using a module
-more experience with unit testing
-"""
-from numpy import matmul
+#!/usr/bin/pYTHON3
+
+"""Defines a matrix multiplication function using NumPy."""
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    return matmul(m_a, m_b)
+    """Return the multiplication of two matrices.
+
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+    """
+
+    return (np.matmul(m_a, m_b))
